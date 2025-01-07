5b44883ebf5c0cccc7143a2c72c7c0b5753a72fd
Martin Cornejo <mart256@gmail.com>
2025-01-08 00:42:56 +0100

    Cleaned main logic

--- src/main/java/com/morsaprogramando/secret_manager/controller/Main.java
+++ src/main/java/com/morsaprogramando/secret_manager/controller/Main.java
@@ -8,19 +8,18 @@ import com.morsaprogramando.secret_manager.services.PasswordManagerService;
 import com.morsaprogramando.secret_manager.view.*;
 
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
         InitialMenu.Action action = InitialMenu.INSTANCE.getAction();
 
         KeystoreData data = switch (action) {
-            case InitialMenu.Open __ -> openKeyStore();
-            case InitialMenu.Create __ -> createKeyStore();
+            case InitialMenu.Open __ -> OpenKeystoreMenu.INSTANCE.getData();
+            case InitialMenu.Create __ -> CreateKeystoreMenu.INSTANCE.createData();
             case InitialMenu.Quit __ -> null;
         };
 
@@ -29,36 +28,34 @@ public class Main {
         PasswordManagerService passwordService = initPasswordService(data.masterPassword());
         FileManagerService fileManagerService = new FileManagerService(data.keyStoreName());
 
-        List<StoredPassword> passwords = new ArrayList<>();
-
-        if (!data.isNew()) {
-            try {
-                passwords.addAll(passwordService.decodePasswords(
-                        fileManagerService.readFile()));
-            } catch (FileNotFoundException e) {
-                Utils.println("The keystore was not found. Try again writing the correct keystore name (without extension).");
-                System.exit(1);
-            } catch (IOException e) {
-                Utils.println("The password is incorrect or the file is corrupted.");
-                System.exit(1);
-            }
-        }
+        List<StoredPassword> passwords = data.isNew() ?
+                new ArrayList<>() :
+                getPasswordsFromKeystore(passwordService, fileManagerService);
 
         KeystoreMenu keystoreMenu = new KeystoreMenu(passwordService, passwords, fileManagerService);
+
         keystoreMenu.render();
-    }
-
-    private static KeystoreData createKeyStore() {
-        return CreateKeystoreMenu.INSTANCE.createData();
-    }
-
-    private static KeystoreData openKeyStore() {
-        return OpenKeystoreMenu.INSTANCE.getData();
     }
 
     private static PasswordManagerService initPasswordService(String masterPassword) {
         EncryptionService encryptionService = EncryptionService.create(masterPassword);
         return new PasswordManagerService(encryptionService);
+    }
+
+    private static List<StoredPassword> getPasswordsFromKeystore(PasswordManagerService passwordService,
+                                                                 FileManagerService fileManagerService) {
+        try {
+            return passwordService.decodePasswords(fileManagerService.readFile());
+
+        } catch (FileNotFoundException e) {
+            Utils.println("The keystore was not found. Try again writing the correct keystore name (without extension).");
+            System.exit(1);
+        } catch (Exception e) {
+            Utils.println("The password is incorrect or the file is corrupted.");
+            System.exit(1);
+        }
+
+        return null;
     }
 
     private static void test() throws Exception {
